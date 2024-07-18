cb86a053a573d99f5721449018745fee253b10c7
mizunomu1v1 <fulltime.furotime@gmail.com>
2024-07-18 23:45:53 +0900

    新規作成

---
+++ tomato/src/main/java/controller/tomatoController.java
@@ -0,0 +1,29 @@
+package controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.puipui.tomato.controller.TomatoApi;
+import com.puipui.tomato.model.TomatoDTO;
+import com.puipui.tomato.model.TomatoForm;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+public class tomatoController implements TomatoApi {
+
+    // private final TomatoService tomatoService;
+
+    @Override
+    public ResponseEntity<TomatoDTO> createTimer(TomatoForm tomatoForm) {
+        // var entity = tomatoService.find(pomodoroTime);
+        var dto = new TomatoDTO();
+
+        // dto.setId(entity.getId());
+        // dto.setTitle(entity.getTitle());
+        dto.setStartTime(0);
+        dto.setEndTime(1);
+        return ResponseEntity.ok(dto);
+    }
+}
