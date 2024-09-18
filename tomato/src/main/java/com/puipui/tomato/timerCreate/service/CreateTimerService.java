21bab9058f13663be9c7ef06717f875044eac79c
mizunomu1v1 <fulltime.furotime@gmail.com>
2024-09-19 01:33:00 +0900

    20240919

--- tomato/src/main/java/com/puipui/tomato/timerCreate/service/CreateTimerService.java
+++ tomato/src/main/java/com/puipui/tomato/timerCreate/service/CreateTimerService.java
@@ -1,9 +1,18 @@
 package com.puipui.tomato.timerCreate.service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.glassfish.jaxb.runtime.v2.runtime.unmarshaller.XsiNilLoader.Array;
+import org.springframework.boot.autoconfigure.web.format.DateTimeFormatters;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.expression.Sets;
 
 import com.puipui.tomato.model.CreateTimerDTO;
 import com.puipui.tomato.model.CreateTimerForm;
+import com.puipui.tomato.model.SetsDTO;
 
 /**
  * ビジネスロジック層
@@ -22,14 +31,51 @@ public class CreateTimerService {
         createTimerEntity.setTotalSets(createTimerForm.getTotalSets());
 
         // システム日時を取得する
-        // TODO
+        // LocalDateTime tmpStartTime = LocalDateTime.now();
+        // DateTimeFormatter formatStartTime = DateTimeFormatter.ofPattern("yyyy/MM/dd
+        // HH:mm:ss.SSS");
+        // String startTime = formatStartTime.format(tmpStartTime);
+        String startTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss.SSS"));
 
         // 2. エラーチェック
         createTimerEntity.validate();
 
-        var dto = new CreateTimerDTO();
+        // 3. 変数作成
+        // sets: setCount分以下を繰り返し配列を作成する
+        ArrayList<SetsDTO> setDto = CreateSets(startTime, createTimerEntity);
 
+        // 4. 4. DB登録
+
+        // 5. レスポンスを返却する
+        var dto = new CreateTimerDTO();
+        dto.setSets(setDto);
         return dto;
+    }
+
+    /**
+     * 変数作成
+     * 
+     * @param startTime
+     * @param createTimerEntity
+     * @return
+     */
+    public ArrayList<SetsDTO> CreateSets(String startTime, CreateTimerEntity createTimerEntity) {
+
+        // setCount分以下を繰り返し配列を作成する
+        ArrayList<SetsDTO> setsDto = new ArrayList<SetsDTO>();
+        Integer totalSets = createTimerEntity.getTotalSets();
+
+        for (Integer i = 0; totalSets > i; i++) {
+            String startWorkTime = startTime;
+            SetsDTO set = new SetsDTO();
+            set.setSet(i);
+            set.setStartWorkTime(startWorkTime);
+            set.setEndWorkTime(startWorkTime);
+            set.endBreakTime(startWorkTime);
+            setsDto.add(set);
+        }
+
+        return setsDto;
     }
 
 }
